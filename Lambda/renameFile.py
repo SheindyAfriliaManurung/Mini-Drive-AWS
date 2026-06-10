4c519a4ee2928e2c2b5f7da3f433ac87079be41f
SheindyAfriliaManurung <sheindymanurung@gmail.com>
2026-06-10 09:52:00 +0700

    Create renameFile.py

---
+++ Lambda/renameFile.py
@@ -0,0 +1,63 @@
+import boto3
+import json
+from datetime import datetime
+
+s3 = boto3.client('s3')
+db = boto3.resource('dynamodb')
+table = db.Table("FilesTable")
+permissions_table = db.Table("FilePermissions")
+BUCKET = "mini-drive-storage-823405633682-us-east-1-an"
+
+def get_email_from_token(event):
+    try:
+        claims = event['requestContext']['authorizer']['jwt']['claims']
+        return claims.get('email', 'Unknown')
+    except:
+        return 'Unknown'
+
+def lambda_handler(event, context):
+    fileid = event['pathParameters']['id']
+    body = json.loads(event['body'])
+    new_name = body['newName']
+    requester = get_email_from_token(event)
+    now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+
+    perm = permissions_table.get_item(Key={"fileId": fileid, "userEmail": requester})
+    if 'Item' not in perm or perm['Item']['role'] not in ['owner', 'editor']:
+        return {
+            "statusCode": 403,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "content-type,authorization",
+                "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS,PUT"
+            },
+            "body": "Only owner or editor can rename files"
+        }
+
+    item = table.get_item(Key={"fileId": fileid})
+    old_key = item['Item']['s3Key']
+    new_key = "uploads/" + new_name
+
+    s3.copy_object(Bucket=BUCKET, CopySource={'Bucket': BUCKET, 'Key': old_key}, Key=new_key)
+    s3.delete_object(Bucket=BUCKET, Key=old_key)
+
+    table.update_item(
+        Key={"fileId": fileid},
+        UpdateExpression="SET fileName = :fn, s3Key = :sk, lastModifiedBy = :mb, lastModifiedAt = :ma",
+        ExpressionAttributeValues={
+            ":fn": new_name,
+            ":sk": new_key,
+            ":mb": requester,
+            ":ma": now
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "content-type,authorization",
+            "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS,PUT"
+        },
+        "body": "renamed"
+    }
