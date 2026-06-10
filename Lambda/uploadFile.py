a2d20fcf61a860efb09cf3a72756c5b097cf3167
SheindyAfriliaManurung <sheindymanurung@gmail.com>
2026-06-10 09:46:46 +0700

    Update uploadFile.py

--- Lambda/uploadFile.py
+++ Lambda/uploadFile.py
@@ -2,10 +2,12 @@ import json
 import boto3
 import uuid
 import base64
+from datetime import datetime
 
 s3 = boto3.client('s3')
 db = boto3.resource('dynamodb')
 table = db.Table("FilesTable")
+permissions_table = db.Table("FilePermissions")
 BUCKET = "mini-drive-storage-823405633682-us-east-1-an"
 
 CONTENT_TYPES = {
@@ -19,6 +21,13 @@ CONTENT_TYPES = {
     'csv': 'text/csv'
 }
 
+def get_email_from_token(event):
+    try:
+        claims = event['requestContext']['authorizer']['jwt']['claims']
+        return claims.get('email', 'Unknown')
+    except:
+        return 'Unknown'
+
 def lambda_handler(event, context):
     body = json.loads(event['body'])
     filename = body['fileName']
@@ -27,6 +36,8 @@ def lambda_handler(event, context):
     ext = filename.split('.')[-1].lower()
     content_type = CONTENT_TYPES.get(ext, 'application/octet-stream')
     is_base64 = body.get('isBase64', False)
+    now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+    uploaded_by = get_email_from_token(event)
 
     if is_base64:
         content = base64.b64decode(body['content'])
@@ -34,18 +45,30 @@ def lambda_handler(event, context):
         content = body['content'].encode('utf-8')
 
     s3.put_object(Bucket=BUCKET, Key=key, Body=content, ContentType=content_type)
+
     table.put_item(Item={
         "fileId": fileid,
         "fileName": filename,
-        "s3Key": key
+        "s3Key": key,
+        "uploadedBy": uploaded_by,
+        "uploadedAt": now,
+        "lastModifiedBy": uploaded_by,
+        "lastModifiedAt": now
+    })
+
+    # Set uploader sebagai owner
+    permissions_table.put_item(Item={
+        "fileId": fileid,
+        "userEmail": uploaded_by,
+        "role": "owner"
     })
 
     return {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "content-type",
-            "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS"
+            "Access-Control-Allow-Headers": "content-type,authorization",
+            "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS,PUT"
         },
         "body": "uploaded"
     }
